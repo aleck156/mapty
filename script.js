b14b6239e19f2f536db0e2930b7b101cbb7b1a65
Aleksander Jaworski <aleck156@gmail.com>
2022-03-31 11:10:06 +0200

    insert boilerplate code from leaflet package, learning!

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ const success = function (pos) {
   console.log(`Your browser do support geolocation`);
   console.log(`Latitude:`.padEnd(12) + `${latitude}`);
   console.log(`Longitude:`.padEnd(12) + `${longitude}`);
+
+  var map = L.map('map').setView([latitude, longitude], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([latitude, longitude])
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
 };
 
 const error = function () {
