67f696b8c88a50d44505e78ea144fcf18c256f70
Aleksander Jaworski <aleck156@gmail.com>
2022-04-03 10:42:26 +0200

    bug fix: workout id property was causing error when generating new instance

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // CLASSES
 class Workout {
   #date = new Date();
-  #id = new Date.now();
+  #id = (Date.now() + '').slice(-10);
 
   constructor(coords, dist, duration) {
     this.coords = coords; // [lat, lng]
